53e529da74e0ff1c7679c64b6d4111e0e886d1e6
Lara-A-O <lara.a.oeltjebruns@gmail.com>
2026-05-14 00:53:23 +0200

    initial commit

---
+++ R/get_city_boundary.R
@@ -0,0 +1,17 @@
+#' Get city boundary from GADM
+#'
+#' @param city Character. City name (e.g. "Aachen"). Only for German cities.
+#' @return sf object with city boundary in WGS84
+#' @export
+get_city_boundary <- function(city) {
+  
+  de <- geodata::gadm("DEU", level = 3, path = tempdir())
+  
+  city_shape <- de[de$NAME_3 == city, ]
+  
+  if (nrow(city_shape) == 0) stop(paste("City not found in GADM database:", city))
+
+  city_sf <- sf::st_as_sf(city_shape)
+  
+  return(city_sf)
+}
